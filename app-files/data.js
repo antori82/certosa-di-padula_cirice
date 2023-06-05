9d2bf21e035608196904777c150e15cc05f2e66a
antori82 <antonio.origlia@unina.it>
2023-06-05 21:58:41 +0200

    Initial commit

---
+++ app-files/data.js
@@ -0,0 +1,262 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-scalone-ellittico_ovest_1",
+      "name": "Scalone ellittico_ovest_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.72900557382264,
+        "pitch": -0.2688557056185914,
+        "fov": 1.3545525999327719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9375024934389868,
+          "pitch": 0.06608049601366339,
+          "rotation": 1.5707963267948966,
+          "target": "1-scalone-ellittico_ovest_2"
+        },
+        {
+          "yaw": 2.830320099366844,
+          "pitch": 0.015056211293805077,
+          "rotation": 1.5707963267948966,
+          "target": "2-scalone-ellittico_ovest_3"
+        },
+        {
+          "yaw": 2.3872077680246164,
+          "pitch": 0.10834292220176422,
+          "rotation": 0,
+          "target": "3-scalone-ellittico_est_1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4800156016464623,
+          "pitch": -0.05345572840556834,
+          "title": "Il sogno dello squarcione&nbsp;",
+          "text": "<div>Rappresentazione del sogno di un prigioniero.&nbsp;</div><div><div>Un gruppo di topi, ritratti con le vesti e le armi di antiche tribù, ballano in un funereo rituale intorno</div><div>ad un palo, sulla cui sommità è aggrappata la vittima del rito, un gatto nero. Alcuni resti ossei ai piedi del palo preannunciano l’epilogo della storia.</div><div>L’interpretazione del sogno sembra abbastanza chiara: i sorci danzanti rappresentano i prigionieri italiani che vincono il nemico, la cui disfatta è incarnata dal gatto nero.</div></div>"
+        },
+        {
+          "yaw": -2.24218575243232,
+          "pitch": -0.299512879669976,
+          "title": "Raffigurazione di un topo in veste da Re",
+          "text": "<div>Il soggetto principale della scena narrativa è nuovamente un topo che, con vesti regali, siede su un trono posto su tre gradini e affiancato da una guardia, anch’ella un animale di non immediata identificazione, ritratta con armi e abiti di rappresentanza. Singolare</div><div>ed estremamente significativa è la presenza di una catena spezzata, lasciata col tratto grezzo dello schizzo, ai piedi del seggio. Il quadro figurativo sembra riferirsi a quello precedente e, in particolare, fissare il momento successivo alla</div><div>vittoria dei sorci sul gatto nero, i quali assumono il potere rappresentato dal topo/re in trono.</div>"
+        },
+        {
+          "yaw": 2.399257589591816,
+          "pitch": -0.40681289302913726,
+          "title": "Sorci verdi",
+          "text": "Composizione&nbsp;in cui il disegno è assente e l’immagine collima con il testo riportante la dicitura “Sorci Verdi” (fig. 7). Data la scritta, i topolini armati della illustrazione onirica potrebbero richiamare alla 205a Squadriglia della Regia Aeronautica appartenente al 41° Gruppo Bombardamento Terrestre<div>del 12° Stormo inquadrato nella III Squadra aerea. Il suo distintivo di reparto erano tre topolini di colore verde, ritti sulle zampe posteriori, l’espressione allegra e di cui due</div><div>intenti a conversare.</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-scalone-ellittico_ovest_2",
+      "name": "Scalone ellittico_ovest_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.2668076725823845,
+        "pitch": -0.036314119804142564,
+        "fov": 1.3545525999327719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.041892965477624,
+          "pitch": -0.041244234563061255,
+          "rotation": 4.71238898038469,
+          "target": "0-scalone-ellittico_ovest_1"
+        },
+        {
+          "yaw": 2.0867013869225133,
+          "pitch": 0.010283599661907061,
+          "rotation": 1.5707963267948966,
+          "target": "2-scalone-ellittico_ovest_3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2758226669804529,
+          "pitch": -0.5169039845296268,
+          "title": "Il sogno dello sguarcione",
+          "text": "<div>La rappresentazione, inserita su un fondale pittorico ottenuto stendendo sul muro una pittura sui toni dell’arancio, scandito dalla ripetizione ritmica di un cereo simbolo grafico astratto, inscena, verosimilmente, il pensiero di un prigioniero, ritratto di profilo sino al busto su uno sfondo bianco di forma tondeggiante, che spera la morte degli occupanti.</div>"
+        },
+        {
+          "yaw": -0.4625359656399226,
+          "pitch": -0.3703561189379343,
+          "title": "Caricatura_soldato",
+          "text": "<div>Raffigurazione dai tratti caricaturali. Il soggetto è un soldato, riconoscibile dalla divisa indossata sulla quale, all’altezza delle cosce, campeggiano le lettere “P” e “W”, ad indicare la condizione di Prisoner of War. Il prigioniero è ritratto di profilo, a figura intera, mentre è intento a sventolare una bandiera, accennata nel disegno solo nel suo contorno, senza altri</div><div>dettagli. L’illustrazione è monocromatica, sui toni del beige e</div><div>si distingue per il tratto deciso, rapido ed essenziale con cui</div><div>l’autore ha accentuato i tratti somatici del personaggio. Particolare attenzione è stata posta dal disegnatore nell’enfatizzare</div><div>la muscolosità del soggetto, la chioma riccia a adornare un viso importante con un naso estremamente pronunciato e</div><div>incorniciato da spesse e lunghe basette.</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-scalone-ellittico_ovest_3",
+      "name": "Scalone ellittico_ovest_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.3503797484657625,
+        "pitch": 0.018818022544358826,
+        "fov": 1.3545525999327719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3143371672136865,
+          "pitch": 0.049348273421523814,
+          "rotation": 4.71238898038469,
+          "target": "1-scalone-ellittico_ovest_2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2901234740489862,
+          "pitch": -0.28998443748776737,
+          "title": "Paessaggio di fantasia",
+          "text": "<div>Raffigurazione di arcate simili a quelle dei chiostri della certosa attraversate con notevole fantasia da due imbarcazioni a vela.</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-scalone-ellittico_est_1",
+      "name": "Scalone ellittico_est_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.7365401810685199,
+        "pitch": -0.09534604731069507,
+        "fov": 1.3545525999327719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.23771318752907433,
+          "pitch": -0.361771316762594,
+          "rotation": 1.5707963267948966,
+          "target": "4-scalone_ellittico_est_2"
+        },
+        {
+          "yaw": -1.930761050913409,
+          "pitch": 0.08823919352283127,
+          "rotation": 0,
+          "target": "0-scalone-ellittico_ovest_1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9257078312379683,
+          "pitch": -0.36507368243532845,
+          "title": "Caricatura di Achille Lauro",
+          "text": "<div>Raffigurazione dai tratti caricaturali, in posizione speculare rispetto</div><div>al sogno dello squarcione. Racchiuso da una cornice disegnata nei suoi ornamenti con abile maestria, a simulare un quadro, è l’effige di un uomo identificato con l’armatore Achille Lauro, uno dei detenuti del Civilian Internee’s Camp di San Lorenzo.</div><div>Ritratto in ambiti eleganti, la figura campeggia su uno sfondo ceruleo alla destra del quale è riprodotta una successione di archi e di volte che richiama alla mente gli spazi del camminamento porticato del chiostro grande.</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-scalone_ellittico_est_2",
+      "name": "Scalone_ellittico_est_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.454772376887128,
+        "pitch": -0.05667856263064763,
+        "fov": 1.3545525999327719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.031443392117696,
+          "pitch": -0.16341804098206936,
+          "rotation": 4.71238898038469,
+          "target": "3-scalone-ellittico_est_1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6288554748767972,
+          "pitch": -0.4516946798334587,
+          "title": "Competizione sportiva",
+          "text": "<div>Tale disegno fornisce uno spaccato sulla vita della prigionia, in cui l’eterno ripetersi di giorni tutti uguali</div><div>e vuoti, l’inedia, la solitudine individuale venivano smorzati da</div><div>momenti collettivi di sport ove, tuttavia, la mortificazione, l’abbrutimento, la costrizione continuavano ad aleggiare. Si individuano così, su due torrette di guardia, alcuni soldati indiani</div><div>– riconoscibili per gli indumenti e ornamenti proposti - urlanti e armati, che vigilano sugli spettatori della gara tra prigionieri il cui anelito di libertà è impedito dal filo spinato che sormonta il recinto, ben visibile nella illustrazione di cui definisce quasi la cornice.</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
